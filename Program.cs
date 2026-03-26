59c15ab1bacec24acc3ba1b2c24a083be1f3adcc
26ALZ01-DanielZea <lopezalvaro16db@gmail.com>
2026-03-25 21:29:22 -0600

    Ejercicios en clase, Act1_Lecc7_Incisos 1-2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
-﻿using System.ComponentModel.Design;
-
-internal class Program
+﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Numeros Positivos y Negativos");
-        //Variables 
-        int num;
-        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("==PROMEDIO DE CALIFICACIONES==");
 
-        if (num > 0) ;
-        Console.WriteLine("Soy un numero POSITIVO");
+        //Variables
+        string materia;
+        int n1, n2, n3, n4, n5;
+        double promedio;
+
+        //Enviar info al usuario
+        Console.Write("Digite una materia");
+        materia = Console.ReadLine();
+        Console.Write("Dame 5 notas");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        n2 = Convert.ToInt32(Console.ReadLine());
+        n3 = Convert.ToInt32(Console.ReadLine());
+        n4 = Convert.ToInt32(Console.ReadLine());
+        n5 = Convert.ToInt32(Console.ReadLine());
+
+        //Proceso
+        promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+        //Evaluar si gano o perdio
+
+        if (promedio >= 70)
+            Console.WriteLine("El promedio es de " + promedio + "y el estudiante aprobo");
         else
-            Console.WriteLine("Soy un numero NEGATIVO");
+            Console.WriteLine("El promedio es de " + promedio + "y el estudiante desaprobo");
     }
 }
